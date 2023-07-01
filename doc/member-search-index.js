b602ed21fa086a1d5226f50c42b16529a50b0fc1
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-07-01 10:55:02 -0300

    Generated javadoc

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"tests","c":"UpdateItineraryFrameTest","l":"setUp()"},{"p":"tests","c":"UpdateItineraryFrameTest","l":"test()"},{"p":"tests","c":"UpdateItineraryFrameTest","l":"UpdateItineraryFrameTest()","u":"%3Cinit%3E()"}];updateSearchResults();
