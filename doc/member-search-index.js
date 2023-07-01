c96c9a9e5b445f9abd28169f807cafc7ae75b13e
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-07-01 13:57:09 -0300

    Deletes Javadoc, which was misconfigured

--- doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"tests","c":"UpdateItineraryFrameTest","l":"setUp()"},{"p":"tests","c":"UpdateItineraryFrameTest","l":"test()"},{"p":"tests","c":"UpdateItineraryFrameTest","l":"UpdateItineraryFrameTest()","u":"%3Cinit%3E()"}];updateSearchResults();
